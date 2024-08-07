5aec5de35d6fe2cf159b8077983bb3e634751ef1
flipnoteman <sammys.email.home@gmail.com>
2024-08-07 16:55:06 -0500

    Added the ability to instantiate an Array. Can dynamically call functions on it to complete operations.

--- src/execution.rs
+++ src/execution.rs
@@ -1,33 +1,41 @@
+use bytemuck;
+use bytemuck::Pod;
+use flume;
+use log::debug;
 use std::borrow::Cow;
 use std::collections::HashMap;
-use std::error::Error;
 use std::ops::Index;
-use bytemuck;
-use wgpu::{Device, Features, InstanceDescriptor, InstanceFlags, MemoryHints, PowerPreference, Queue, RequestDeviceError, ShaderModule};
-use flume;
 use wgpu::util::DeviceExt;
-use log::{info, debug};
+use wgpu::{
+    Buffer, Device, Features, InstanceDescriptor, InstanceFlags, MemoryHints, PowerPreference,
+    Queue, ShaderModule,
+};
 
 pub type ShaderResources = HashMap<String, ShaderModule>;
 
+/// GpuHandle
+/// This will hold our [Device] and [Queue] for later executions
 #[derive(Debug)]
 pub struct GpuHandle {
-    pub device: Device,
-    pub queue: Queue,
+    pub device: Box<Device>,
+    pub queue: Box<Queue>,
 }
 
 impl GpuHandle {
     pub fn new(device: Device, queue: Queue) -> Self {
         GpuHandle {
-            device,
-            queue,
+            device: Box::new(device),
+            queue: Box::new(queue),
         }
     }
 }
 
+#[derive(Debug)]
 pub struct Executor {
-    pub adapter: Option<GpuHandle>,
-    pub shaders: Option<ShaderResources>,
+    pub adapter: Option<Box<GpuHandle>>,
+    pub shaders: Option<Box<ShaderResources>>,
+    storage_buffer: Option<Buffer>,
+    staging_buffer: Option<Buffer>,
 }
 
 impl Default for Executor {
@@ -35,6 +43,8 @@ impl Default for Executor {
         Executor {
             adapter: None,
             shaders: None,
+            storage_buffer: None,
+            staging_buffer: None,
         }
     }
 }
@@ -45,71 +55,214 @@ impl Executor {
     pub async fn new(shader_path_directory: &str) -> Result<Self, String> {
         let mut ex = Executor::default();
         let adapter = Executor::get_adapter_info().await?;
-        let shaders = Executor::add_shader_modules_from_directory(&adapter.device, shader_path_directory).await;
+        // TODO: Switch this to add shader modules only when you stage the associated function
+        let shaders =
+            Executor::add_shader_modules_from_directory(&adapter.device, shader_path_directory)
+                .await;
 
-        ex.shaders = shaders;
-        ex.adapter = Some(adapter);
+        if let Some(shaders) = shaders {
+            ex.shaders = Some(Box::new(shaders))
+        } else {
+            ex.shaders = None
+        }
+        ex.adapter = Some(Box::new(adapter));
+
         Ok(ex)
     }
 
     // Prints Executor fields for debugging. Must have log_level set to debug
     pub fn info(&self) {
-        log::debug!("{:?}", self.shaders);
-        log::debug!("{:?}", self.adapter);
+        debug!("{:?}", self.shaders);
+        debug!("{:?}", self.adapter);
     }
 
+    /// Sets up storage and staging (input, output) buffers and adds them to the executor
+    pub async fn setup_buffers<T>(&mut self, data: &[T]) -> Result<(), String>
+    where
+        T: Pod,
+    {
+        let Some(ref adapter) = self.adapter else {
+            return Err("Not operations loaded".parse().unwrap());
+        };
+        // Instantiates buffer with data (`numbers`).
+        // Usage allowing the buffer to be:
+        //   A storage buffer (can be bound within a bind group and thus available to a shader).
+        //   The destination of a copy.
+        //   The source of a copy.
+        let storage_buffer = adapter
+            .device
+            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Storage Buffer"),
+                contents: bytemuck::cast_slice::<T, u8>(&data),
+                usage: wgpu::BufferUsages::STORAGE
+                    | wgpu::BufferUsages::COPY_DST
+                    | wgpu::BufferUsages::COPY_SRC,
+            });
 
+        // Instantiates buffer without data.
+        // `usage` of buffer specifies how it can be used:
+        //   `BufferUsages::MAP_READ` allows it to be read (outside the shader).
+        //   `BufferUsages::COPY_DST` allows it to be the destination of the copy.
+        let staging_buffer = adapter.device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: storage_buffer.size(),
+            usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        self.storage_buffer = Some(storage_buffer);
+        self.staging_buffer = Some(staging_buffer);
+
+        Ok(())
+    }
+
+    /// Test function.
+    /// Doubles the array input
+    pub async fn test_fn(&self) -> Result<(), String> {
+        // Instantiate our Executor
+        let Some(adapter) = self.adapter.as_ref() else {
+            return Err("Not operations loaded".parse().unwrap());
+        };
+        let device = &adapter.device;
+        let queue = &adapter.queue;
+        let Some(shaders) = self.shaders.as_ref() else {
+            return Err("Not operations loaded".parse().unwrap());
+        };
+        let storage_buffer = self.storage_buffer.as_ref().unwrap();
+        let staging_buffer = self.staging_buffer.as_ref().unwrap();
+
+        // A pipeline specifies the operation of a shader
+        // Instantiates the pipeline.
+        let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: None,
+            layout: None,
+            module: shaders.index("double"),
+            entry_point: "main",
+            compilation_options: Default::default(),
+            cache: None,
+        });
+
+        // A bind group defines how buffers are accessed by operations.
+        // It is to WebGPU what a descriptor set is to Vulkan.
+        // `binding` here refers to the `binding` of a buffer in the shader (`layout(set = 0, binding = 0) buffer`).
+        // Instantiates the bind group, once again specifying the binding of buffers.
+        let bind_group_layout = compute_pipeline.get_bind_group_layout(0);
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: storage_buffer.as_entire_binding(),
+            }],
+        });
+        // A command encoder executes one or many pipelines.
+        // It is to WebGPU what a command buffer is to Vulkan.
+        let mut encoder =
+            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        {
+            let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+                label: None,
+                timestamp_writes: None,
+            });
+            cpass.set_pipeline(&compute_pipeline);
+            cpass.set_bind_group(0, &bind_group, &[]);
+            cpass.insert_debug_marker("");
+            cpass.dispatch_workgroups(storage_buffer.size() as u32, 1, 1); // Number of cells to run, the (x,y,z) size of item being processed
+        }
+        // Sets adds copy operation to command encoder.
+        // Will copy data from storage buffer on GPU to staging buffer on CPU.
+        encoder.copy_buffer_to_buffer(
+            &storage_buffer,
+            0,
+            &staging_buffer,
+            0,
+            staging_buffer.size(),
+        );
+
+        // Submits command encoder for processing
+        queue.submit(Some(encoder.finish()));
+
+        // Note that we're not calling `.await` here.
+        let buffer_slice = staging_buffer.slice(..);
+        // Sets the buffer up for mapping, sending over the result of the mapping back to us when it is finished.
+        let (sender, receiver) = flume::bounded(1);
+        buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
+
+        // Poll the device in a blocking manner so that our future resolves.
+        // In an actual application, `device.poll(...)` should
+        // be called in an event loop or on another thread.
+        device.poll(wgpu::Maintain::wait()).panic_on_timeout();
+
+        // Awaits until `buffer_future` can be read from
+        if let Ok(Ok(())) = receiver.recv_async().await {
+            // Gets contents of buffer
+            let data = buffer_slice.get_mapped_range();
+            // Since contents are got in bytes, this converts these bytes back to u32
+            let result: Vec<u32> = bytemuck::cast_slice(&data).to_vec();
+
+            // With the current interface, we have to make sure all mapped views are
+            // dropped before we unmap the buffer.
+            drop(data);
+            staging_buffer.unmap(); // Unmaps buffer from memory
+                                    // If you are familiar with C++ these 2 lines can be thought of similarly to:
+                                    //   delete myPointer;
+                                    //   myPointer = NULL;
+                                    // It effectively frees the memory
+
+            println!("Result = {:?}", result);
+            Ok(())
+        } else {
+            panic!("failed to run compute on gpu!");
+        }
+    }
 }
 
 // Private impl
 impl Executor {
-
     /// Get device description. Should return the highest performance device on a system. Should only be called once unless you need to request another adapter.
-    async fn get_adapter_info() -> Result<GpuHandle, String>{
-
+    async fn get_adapter_info() -> Result<GpuHandle, String> {
         // Creates adapters and surfaces using the information in the ```InstanceDescriptor```
-        let instance = wgpu::Instance::new(
-            InstanceDescriptor {
-                backends: wgpu::Backends::PRIMARY,
-                flags: InstanceFlags::empty(), // Instance flags for debugging.
-                dx12_shader_compiler: Default::default(), // Select which DX12 compiler to use.
-                gles_minor_version: Default::default(), // Select which minor version of Open GL to use.
-            }
-        );
+        let instance = wgpu::Instance::new(InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            flags: InstanceFlags::empty(), // Instance flags for debugging.
+            dx12_shader_compiler: Default::default(), // Select which DX12 compiler to use.
+            gles_minor_version: Default::default(), // Select which minor version of Open GL to use.
+        });
 
         // Gives us a handle to all gpu compute adapters with the given ```RequestAdapterOptions```
-        let Some(adapter) = instance.request_adapter(
-            &wgpu::RequestAdapterOptions {
+        let Some(adapter) = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
                 power_preference: PowerPreference::HighPerformance, // HighPerformance will tell it to return adapters that offer higher performance, like GPUs.
                 force_fallback_adapter: false, // If true, will force WGPU to use an adapter that is supported by all hardware.
                 compatible_surface: None, // If given a surface (like a window / display) it will return adapters that can present to that surface.
-            }
-        ).await else {
-            return Err("Found no adapters.".parse().unwrap())
+            })
+            .await
+        else {
+            return Err("Found no adapters.".parse().unwrap());
         };
 
-        log::debug!("Adapter(s) = {:?}", adapter.get_info());
+        debug!("Adapter(s) = {:?}", adapter.get_info());
 
-        let (device, queue) = adapter.request_device(
-            &wgpu::DeviceDescriptor {
-                label: Some("Device 1"), // Debug label
-                required_features: Features::empty(), // Define a list of features that the device must implement.
-                required_limits: Default::default(), // Defines a list of limits of certain types of resources that we can create.
-                memory_hints: MemoryHints::MemoryUsage, // Defines memory allocation hints for our device.
-            },
-            None // Typically a path used for tracing api calls.
-        ).await.expect("Error requesting device.");
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: Some("Device 1"),                // Debug label
+                    required_features: Features::empty(), // Define a list of features that the device must implement.
+                    required_limits: Default::default(), // Defines a list of limits of certain types of resources that we can create.
+                    memory_hints: MemoryHints::MemoryUsage, // Defines memory allocation hints for our device.
+                },
+                None, // Typically a path used for tracing api calls.
+            )
+            .await
+            .expect("Error requesting device.");
 
-        Ok(GpuHandle {
-            device,
-            queue
-        })
+        Ok(GpuHandle::new(device, queue))
     }
 
-    /// Returns a list of ShaderModules after being given a list of shader paths
+    /// Returns a list of [ShaderModule] after being given a list of shader paths
     async fn add_shader_modules<'a>(
         device: &Device,
-        shader_paths: &[String]
+        shader_paths: &[String],
     ) -> Option<ShaderResources> {
         let mut shader_module_hm = HashMap::new();
 
@@ -117,34 +270,43 @@ impl Executor {
         for path in shader_paths {
             let cs_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
                 label: Some(path),
-                source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(&*std::fs::read_to_string(path).unwrap())),
+                source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(
+                    &*std::fs::read_to_string(path).unwrap(),
+                )),
             });
             shader_module_hm.insert(path.to_owned(), cs_module);
         }
         Some(shader_module_hm)
     }
 
+    /// Returns a list of [ShaderModule]s from a given directory
     async fn add_shader_modules_from_directory<'a>(
         device: &Device,
-        shaders_directory: &str
+        shaders_directory: &str,
     ) -> Option<ShaderResources> {
         let mut shader_module_hm = HashMap::new();
 
-        let shader_paths = match std::fs::read_dir(shaders_directory){
-            Ok(s) => {s}
-            Err(_) => {return None}
-        }.map(|path| {
-            path.unwrap().path().into_os_string().into_string().unwrap()
-        }).collect::<Vec<String>>();
+        let shader_paths = match std::fs::read_dir(shaders_directory) {
+            Ok(s) => s,
+            Err(_) => return None,
+        }
+        .map(|path| path.unwrap().path().into_os_string().into_string().unwrap())
+        .collect::<Vec<String>>();
 
         // Iterate paths and create shader modules out of them
         for path in shader_paths.iter() {
             let file_name = path
-                .strip_prefix(shaders_directory).unwrap()
-                .strip_prefix("\\").unwrap()
-                .strip_suffix(".wgsl").unwrap();
+                .strip_prefix(shaders_directory)
+                .unwrap()
+                .strip_prefix("\\")
+                .unwrap()
+                .strip_suffix(".wgsl")
+                .unwrap();
 
-            let shader: Cow<str> = Cow::from(std::fs::read_to_string(path).expect(format!("Could not read file contents from: {}", path).as_str()));
+            let shader: Cow<str> = Cow::from(
+                std::fs::read_to_string(path)
+                    .expect(format!("Could not read file contents from: {}", path).as_str()),
+            );
             let cs_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
                 label: Some(file_name),
                 source: wgpu::ShaderSource::Wgsl(shader),
--- src/lib.rs
+++ src/lib.rs
@@ -1,135 +1,36 @@
-mod execution;
 
-use std::ops::Index;
+use bytemuck::Pod;
 use execution::Executor;
-use wgpu::util::DeviceExt;
+
+mod execution;
 
 const SHADERS_PATH: &str = "./Luma/operations";
 
+#[derive(Debug)]
 pub struct Array {
-    
+    dimensions: [u64; 4],
+    executor: Executor,
 }
 
 impl Array {
-    pub async fn new() -> Result<Self, String> {
+    pub async fn new<T>(dimensions: &[u64; 4], data: &[T]) -> Result<Self, String>
+    where
+        T: Pod,
+    {
         // Instantiate our Executor
-        let mut ex = Box::new(Executor::new(SHADERS_PATH).await.unwrap());
-        let adapter = &ex.adapter.as_ref().unwrap();
-        let device = &adapter.device;
-        let queue = &adapter.queue;
-        let Some(ref shaders) = ex.shaders else {
-            return Err("Not operations loaded".parse().unwrap());
-        };
+        let mut ex = Box::new(Executor::new(SHADERS_PATH).await?);
 
-        // ex.info();
+        // Setup input output buffers with our data
+        // TODO: Incorporate the dimensions array
+        ex.setup_buffers(data).await?;
 
-        // simple_logger::init_with_level(Level::Info).expect("Could not execute logger");
-        let numbers = vec![1u32, 2, 3, 4];
+        Ok(Array {
+            dimensions: *dimensions,
+            executor: *ex,
+        })
+    }
 
-        // Instantiates buffer with data (`numbers`).
-        // Usage allowing the buffer to be:
-        //   A storage buffer (can be bound within a bind group and thus available to a shader).
-        //   The destination of a copy.
-        //   The source of a copy.
-        let storage_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Storage Buffer"),
-            contents: bytemuck::cast_slice(&numbers),
-            usage: wgpu::BufferUsages::STORAGE
-                | wgpu::BufferUsages::COPY_DST
-                | wgpu::BufferUsages::COPY_SRC,
-        });
-
-        // Instantiates buffer without data.
-        // `usage` of buffer specifies how it can be used:
-        //   `BufferUsages::MAP_READ` allows it to be read (outside the shader).
-        //   `BufferUsages::COPY_DST` allows it to be the destination of the copy.
-        let staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            label: None,
-            size: storage_buffer.size(),
-            usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
-            mapped_at_creation: false,
-        });
-
-
-        // A bind group defines how buffers are accessed by operations.
-        // It is to WebGPU what a descriptor set is to Vulkan.
-        // `binding` here refers to the `binding` of a buffer in the shader (`layout(set = 0, binding = 0) buffer`).
-
-        // A pipeline specifies the operation of a shader
-
-        // Instantiates the pipeline.
-        let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
-            label: None,
-            layout: None,
-            module: shaders.index("double"),
-            entry_point: "main",
-            compilation_options: Default::default(),
-            cache: None,
-        });
-
-        // Instantiates the bind group, once again specifying the binding of buffers.
-        let bind_group_layout = compute_pipeline.get_bind_group_layout(0);
-        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: None,
-            layout: &bind_group_layout,
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: storage_buffer.as_entire_binding(),
-            }],
-        });
-
-        // A command encoder executes one or many pipelines.
-        // It is to WebGPU what a command buffer is to Vulkan.
-        let mut encoder =
-            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-        {
-            let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-                label: None,
-                timestamp_writes: None,
-            });
-            cpass.set_pipeline(&compute_pipeline);
-            cpass.set_bind_group(0, &bind_group, &[]);
-            cpass.insert_debug_marker("");
-            cpass.dispatch_workgroups(numbers.len() as u32, 1, 1); // Number of cells to run, the (x,y,z) size of item being processed
-        }
-        // Sets adds copy operation to command encoder.
-        // Will copy data from storage buffer on GPU to staging buffer on CPU.
-        encoder.copy_buffer_to_buffer(&storage_buffer, 0, &staging_buffer, 0, staging_buffer.size());
-
-        // Submits command encoder for processing
-        queue.submit(Some(encoder.finish()));
-
-        // Note that we're not calling `.await` here.
-        let buffer_slice = staging_buffer.slice(..);
-        // Sets the buffer up for mapping, sending over the result of the mapping back to us when it is finished.
-        let (sender, receiver) = flume::bounded(1);
-        buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
-
-        // Poll the device in a blocking manner so that our future resolves.
-        // In an actual application, `device.poll(...)` should
-        // be called in an event loop or on another thread.
-        device.poll(wgpu::Maintain::wait()).panic_on_timeout();
-
-        // Awaits until `buffer_future` can be read from
-        if let Ok(Ok(())) = receiver.recv_async().await {
-            // Gets contents of buffer
-            let data = buffer_slice.get_mapped_range();
-            // Since contents are got in bytes, this converts these bytes back to u32
-            let result: Vec<u32> = bytemuck::cast_slice(&data).to_vec();
-
-            // With the current interface, we have to make sure all mapped views are
-            // dropped before we unmap the buffer.
-            drop(data);
-            staging_buffer.unmap(); // Unmaps buffer from memory
-            // If you are familiar with C++ these 2 lines can be thought of similarly to:
-            //   delete myPointer;
-            //   myPointer = NULL;
-            // It effectively frees the memory
-
-            println!("Result = {:?}", result);
-        } else {
-            panic!("failed to run compute on gpu!");
-        }
-        Ok(Array {})
+    pub async fn double_test(&self) -> Result<(), String> {
+        self.executor.test_fn().await
     }
 }
